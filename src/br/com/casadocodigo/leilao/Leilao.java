5a0ae2bbaafdf981c176df9cb8bafef18c0fb3d7
Gabriel <gabrielborc@gmail.com>
2019-10-22 09:32:48 -0300

    Finalizado o capítulo 02

--- src/br/com/casadocodigo/leilao/Leilao.java
+++ src/br/com/casadocodigo/leilao/Leilao.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.leilao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Leilao {
 
@@ -10,10 +11,6 @@ public class Leilao {
 
     public Leilao(String nome) {
         this.nome = nome;
-    }
-
-    public void propoe(Lance lance) {
-        lances.add(lance);
     }
 
     public String getNome() {
@@ -30,6 +27,29 @@ public class Leilao {
 
     public void setLances(List<Lance> lances) {
         this.lances = lances;
+    }
+
+    public void propoe(Lance lance) {
+        if (lances.isEmpty() || podeDarLance(lance.getUsuario())) {
+            lances.add(lance);
+        }
+    }
+
+    private Lance ultimoLanceDado() {
+        return lances.get(lances.size()-1);
+    }
+
+    private int qtdDelancesDo(Usuario usuario) {
+        int total = 0;
+        for (Lance l : lances) {
+            if (l.getUsuario().equals(usuario)) total++;
+        }
+        return total;
+    }
+
+    private boolean podeDarLance(Usuario usuario) {
+        return !ultimoLanceDado().getUsuario().equals(usuario)
+                && qtdDelancesDo(usuario) < 5;
     }
 
 }
